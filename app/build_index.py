bf2a357fb4e2d2e0f1f7f7393cb6b1e1afe0e834
Chris Connett <cjc@google.com>
2017-10-29 20:22:32 -0400

    Use pyparsing to parse the PHP fragments.

--- app/build_index.py
+++ app/build_index.py
@@ -1,16 +1,20 @@
 """Build an index for ALTTP project."""
 
+from __future__ import print_function
 import collections
 import os
 import pprint
 import re
+
+import pyparsing as p
+s = p.Suppress
 all_items = [item_line.strip() for item_line in open('items.txt').readlines()]
 
 
 def GetMentionedThings(location_pattern, things, text):
   match = re.search(location_pattern, text)
   if match:
-    matched_text = match.group(1)
+    matched_text = match.group(2)
     for item in things:
       if item in matched_text:
         yield item
@@ -19,29 +23,68 @@ def GetMentionedThings(location_pattern, things, text):
 def WalkSources():
   for root, _, files in os.walk('Region/'):
     for filename in files:
-      if filename.endswith('.php'):
-        yield open(os.path.join(root, filename)).read()
+      path = os.path.join(root, filename)
+      if path.endswith('.php'):
+        yield (open(path).read(), path[:-len('.php')])
+
+
+php_expr = p.Forward().setName('expr')
+p.quotedString.addParseAction(p.removeQuotes)
+item_in_locations = (
+    s('$locations->itemInLocations(Item::get(') +
+    p.quotedString.copy().setResultsName('item') + s(')') + s(',') + s('[') +
+    p.Group(p.quotedString + p.ZeroOrMore(',' + p.quotedString) + p.Optional(
+        ',')).setResultsName('allowable_locations') + s(']') + s(')'))
+php_atom = (
+    (s('$items->has(') + p.quotedString + p.Optional(s(',') + p.Word(p.nums)) +
+     s(')')).setResultsName('has') |
+    (s('$this->world->getRegion(') + p.quotedString +
+     s(')->canEnter($locations, $items)')).setResultsName('region') |
+    (s('$items->') + p.Word(p.alphas) + s('()')).setResultsName('function') |
+    item_in_locations | (s('(') + php_expr + s(')')))
+php_literal = p.Group(p.Optional('!') + php_atom).setResultsName('literal')
+php_and = (php_literal + p.ZeroOrMore('&&' + php_literal))
+php_or = (php_and + p.ZeroOrMore('||' + php_and)).setResultsName('ored')
+
+php_expr <<= php_or
+php_return = s('return') + php_expr + s(';')
 
 
 def BuildIndex():
+  """Parse stupid PHP."""
+  errors = []
   index = collections.defaultdict(set)
-  for source in WalkSources():
+  for source, region in WalkSources():
     for location in open('locations.txt'):
       location = location.strip()
-      pattern = re.compile(r'\["{}"\]->setRequirements\(.*?\{{(.*?)\}}'.format(
-          re.escape(location)), re.DOTALL | re.MULTILINE)
-      queue = collections.deque(
-          GetMentionedThings(pattern, all_items + list(compound_items.keys()),
-                             source))
-      done = set()
-      while queue:
-        item = queue.pop()
-        if item in all_items:
-          index[item].add(location)
-        else:
-          if item not in done:
-            queue.extendleft(compound_items[item])
-        done.add(item)
+      pattern = re.compile(
+          r'\["({})"\]->setRequirements\(.*?\{{(.*?)\}}'.format(
+              re.escape(location)), re.DOTALL | re.MULTILINE)
+      match = pattern.search(source)
+      if match:
+        found_location = match.group(1)
+        body = match.group(2)
+        try:
+          print(region, found_location)
+          print(php_return.parseString(body))
+        except p.ParseException:
+          errors.append((found_location, body))
+
+      # queue = collections.deque(
+      #     GetMentionedThings(pattern, all_items + list(compound_items.keys()),
+      #                        source))
+      # done = set()
+      # while queue:
+      #   item = queue.pop()
+      #   if item in all_items:
+      #     index[item].add(location)
+      #   else:
+      #     if item not in done:
+      #       queue.extendleft(compound_items[item])
+      #   done.add(item)
+  print(len(errors))
+  for e in errors:
+    print(e[0], e[1])
   return index
 
 
