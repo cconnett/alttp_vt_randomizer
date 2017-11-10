9429e461c3998833717cc5b8b1712ece366eef19
Chris Connett <cjc@google.com>
2017-11-10 17:54:19 -0500

    Handle more cases in the php parser.
    Conflicts:
    	app/build_index.py

--- app/build_index.py
+++ app/build_index.py
@@ -3,7 +3,6 @@
 from __future__ import print_function
 import collections
 import os
-import pprint
 import re
 
 import pyparsing as p
@@ -34,13 +33,24 @@ item_in_locations = (
     p.quotedString.copy().setResultsName('item') + s(')') + s(',') + s('[') +
     p.Group(p.quotedString + p.ZeroOrMore(',' + p.quotedString) + p.Optional(
         ',')).setResultsName('allowable_locations') + s(']') + s(')'))
-php_atom = (
-    (s('$items->has(') + p.quotedString + p.Optional(s(',') + p.Word(p.nums)) +
-     s(')')).setResultsName('has') |
-    (s('$this->world->getRegion(') + p.quotedString +
-     s(')->canEnter($locations, $items)')).setResultsName('region') |
-    (s('$items->') + p.Word(p.alphas) + s('()')).setResultsName('function') |
-    item_in_locations | (s('(') + php_expr + s(')')))
+
+items_has = (s('$items->has(') + p.quotedString +
+             p.Optional(s(',') + p.Word(p.nums)) + s(')')).setResultsName('has')
+region_can_enter = (
+    s('$this->world->getRegion(') + p.quotedString +
+    s(')->canEnter($locations, $items)')).setResultsName('region')
+function_call = (
+    s('$items->') + p.Word(p.alphas) + s('()')).setResultsName('function')
+swordless = p.Literal("config('game-mode') == 'swordless'")
+open_or_swordless = "in_array(config('game-mode'), ['open', 'swordless'])"
+
+is_a = 'is_a($item, Item\\' + p.Word(p.alphas) + '::class)'
+boss_normal_location = p.Literal(
+    "!$this->world->config('region.bossNormalLocation', true)")
+parenthesized_expr = (s('(') + php_expr + s(')'))
+php_atom = (items_has | region_can_enter | function_call | item_in_locations |
+            swordless | open_or_swordless | is_a | boss_normal_location |
+            parenthesized_expr)
 php_literal = p.Group(p.Optional('!') + php_atom).setResultsName('literal')
 php_and = (php_literal + p.ZeroOrMore('&&' + php_literal))
 php_or = (php_and + p.ZeroOrMore('||' + php_and)).setResultsName('ored')
@@ -81,71 +91,10 @@ def BuildIndex():
       #     if item not in done:
       #       queue.extendleft(compound_items[item])
       #   done.add(item)
+  print('===Errors===')
   print(len(errors))
   for e in errors:
     print(e[0], e[1])
   return index
 
-
-compound_items = {
-    'Desert Palace': [
-        'BookOfMudora', 'MagicMirror', 'canLiftDarkRocks', 'canFly'
-    ],
-    'East Dark World Death Mountain': [
-        'canLiftDarkRocks',
-        'East Death Mountain',
-    ],
-    'East Death Mountain': [
-        'West Death Mountain', 'Hammer', 'MagicMirror', 'Hookshot'
-    ],
-    'Ganons Tower': [
-        'MoonPearl', 'Crystal1', 'Crystal2', 'Crystal3', 'Crystal4', 'Crystal5',
-        'Crystal6', 'Crystal7', 'East Dark World Death Mountain'
-    ],
-    'Mire': ['canFly', 'canLiftDarkRocks'],
-    'Misery Mire': [
-        'Bombos', 'Ether', 'Quake', 'MoonPearl', 'PegasusBoots', 'Hookshot',
-        'Mire', 'hasSword'
-    ],
-    'North East Dark World': [
-        'DefeatAgahnim', 'Hammer', 'canLiftRocks', 'MoonPearl',
-        'canLiftDarkRocks', 'Flippers'
-    ],
-    'North West Dark World': [
-        'North East Dark World', 'Hookshot', 'Flippers', 'Hammer',
-        'canLiftRocks', 'canLiftDarkRocks', 'MoonPearl'
-    ],
-    'South Dark World': [
-        'MoonPearl', 'canLiftDarkRocks', 'Hammer', 'canLiftRocks',
-        'DefeatAgahnim', 'Hookshot', 'Flippers', 'MagicMirror', 'PegasusBoots'
-    ],
-    'Tower of Hera': ['Mirror', 'Hookshot', 'Hammer', 'West Death Mountain'],
-    'Turtle Rock': [
-        'Bombos', 'Ether', 'Quake', 'hasSword', 'MoonPearl', 'canLiftDarkRocks',
-        'Hammer', 'East Death Mountain', 'CaneOfSomaria'
-    ],
-    'West Death Mountain': ['canFly', 'canLiftRocks', 'Lamp'],
-    'canLiftRocks': ['PowerGlove', 'ProgressiveGlove', 'TitansMitt'],
-    'canLiftDarkRocks': ['TitansMitt', 'ProgressiveGlove'],
-    'canLightTorches': ['FireRod', 'Lamp'],
-    'canMeltThings': ['FireRod', 'Bombos', 'hasSword'],
-    'canFly': ['OcarinaActive', 'OcarinaInactive'],
-    'canSpinSpeed': ['PegasusBoots', 'hasSword', 'Hookshot'],
-    'canShootArrows': ['Bow', 'BowAndArrows', 'BowAndSilverArrows'],
-    'canBlockLasers': ['MirrorShield', 'ProgressiveShield'],
-    'canExtendMagic': ['HalfMagic', 'QuarterMagic', 'hasABottle'],
-    'glitchedLinkInDarkWorld': ['MoonPearl', 'hasABottle'],
-    'hasSword': [
-        'L1Sword', 'L1SwordAndShield', 'ProgressiveSword', 'hasUpgradedSword'
-    ],
-    'hasUpgradedSword': [
-        'L2Sword', 'MasterSword', 'L3Sword', 'L4Sword', 'ProgressiveSword'
-    ],
-    'hasABottle': [
-        'BottleWithBee', 'BottleWithFairy', 'BottleWithRedPotion',
-        'BottleWithGreenPotion', 'BottleWithBluePotion', 'Bottle',
-        'BottleWithGoldBee'
-    ],
-}
-
-pprint.pprint({key: val for key, val in BuildIndex().items()})
+end = BuildIndex()
