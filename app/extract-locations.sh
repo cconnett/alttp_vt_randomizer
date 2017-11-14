b1dad4723a4f93b963b6f81e868c0bb4a18ce930
Chris Connett <chris@connett.net>
2017-11-14 00:32:47 -0500

    Fix the location extraction pattern.

--- app/extract-locations.sh
+++ app/extract-locations.sh
@@ -1,1 +1,1 @@
-ack 'this->locations\["(.*?)"\]->setReq' --output='$1' | cut -d: -f3 | sort -u > locations.txt
+ack 'this->locations\["(.*?)"\]->setItem' --output='$1' | cut -d: -f3 | sort -u > locations.txt
