82c64d5984fc0ab0956474fc7de0e4e8c720d36a
Chris Connett <cjc@google.com>
2017-11-27 11:23:19 -0500

    Connect untwister's php-mt_rand implementation to our mt_rand.

--- app/mt_rand.cc
+++ app/mt_rand.cc
@@ -1,16 +1,13 @@
+#include "PHP_mt19937.h"
+
 #include "mt_rand.h"
+
+PHP_mt19937 generator;
 
 int scale(int n, int min, int max) {
   return (int)(min + (max - min + 1.0) * (((double)n) / (1 << 31)));
 }
 
-int mt_rand(int min, int max) { return scale(19283, min, max); }
+void php_srand(int seed) { generator.seed(seed); }
 
-void mt_shuffle(int n, int *array) {
-  for (unsigned int i = n - 1; i > 0; i--) {
-    int j = mt_rand(0, i);
-    int tmp = array[i];
-    array[i] = array[j];
-    array[j] = tmp;
-  }
-}
+uint mt_rand(uint min, uint max) { return scale(generator.random(), min, max); }
---
+++ app/untwister.BUILD
@@ -0,0 +1,17 @@
+cc_library(
+    name = "php-mt_rand",
+    srcs = [
+        "ConsoleColors.h",
+        "PHP_mt19937.cpp",
+        "PHP_mt19937.h",
+    ],
+    visibility = ["//visibility:public"],
+    deps = [":prng"],
+)
+
+cc_library(
+    name = "prng",
+    srcs = [
+        "PRNG.h",
+    ],
+)
