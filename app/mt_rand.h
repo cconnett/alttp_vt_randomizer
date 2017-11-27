3e723ea1facf58c5579f5bdfaef65e191e8ffba9
Chris Connett <cjc@google.com>
2017-11-27 11:23:19 -0500

    Connect untwister's php-mt_rand implementation to our mt_rand.

--- app/mt_rand.h
+++ app/mt_rand.h
@@ -1,2 +1,11 @@
-int mt_rand(int low, int high);
-void mt_shuffle(int n, int *array);
+uint mt_rand(uint low, uint high);
+
+template <typename T>
+void mt_shuffle(uint n, T *array) {
+  for (uint i = n - 1; i > 0; i--) {
+    uint j = mt_rand(0, i);
+    T tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+  }
+}
