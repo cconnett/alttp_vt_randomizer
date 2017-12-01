4a517a1aaeda319dee00bda1b40ecdd77e3584f5
Chris Connett <cjc@google.com>
2017-11-30 19:51:01 -0500

    More progression.

--- app/mt_rand.h
+++ app/mt_rand.h
@@ -1,10 +1,11 @@
+#include <cassert>
 #include <vector>
 
 uint mt_rand(uint low, uint high);
 void php_srand(int seed);
 
 template <typename T>
-void mt_shuffle(size_t n, T *array) {
+void mt_shuffle(T *array, size_t n) {
   for (uint i = n - 1; i > 0; i--) {
     uint j = mt_rand(0, i);
     T tmp = array[i];
@@ -14,12 +15,13 @@ void mt_shuffle(size_t n, T *array) {
 }
 
 template <typename T>
-std::vector<T> mt_sample(size_t b, size_t n, T *array) {
+std::vector<T> mt_sample(T *population, size_t popsize, size_t k) {
+  assert(k <= popsize);
   std::vector<T> original, ret;
-  for (uint i = 0; i < n; i++) {
-    original.push_back(array[i]);
+  for (uint i = 0; i < popsize; i++) {
+    original.push_back(population[i]);
   }
-  while (b-- > 0) {
+  while (k-- > 0) {
     uint pos = mt_rand(0, original.size());
     ret.push_back(original[pos]);
     original.erase(original.begin() + pos);
