8747da97728b1e7eaa6fd3af7411d5df38db8bf7
Chris Connett <cjc@google.com>
2017-12-27 18:27:11 -0500

    Remove PHP debugging.

--- app/Filler/RandomAssumed.php
+++ app/Filler/RandomAssumed.php
@@ -61,8 +61,6 @@ class RandomAssumed extends Filler {
 
 			$fill_location = $fillable_locations->first();
 
-			printf("%s := %s\n", $fill_location->getName(), $item->getName());
-
 			$fill_location->setItem($item);
 		}
 	}
