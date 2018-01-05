bb4da84eaa2bfe692bc202c876c8b6980b87ee3f
Chris Connett <cjc@google.com>
2018-01-04 19:26:26 -0500

    Filter more aggressively in the test so that it can be run with a debug binary. Also don't sort.

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
 function runtest {
   seed="$1"
   shift
-  php artisan -v alttp:randomize --skip-md5 /tmp/rom /tmp --no-rom --spoiler --seed=${seed} --mode=open > /dev/null
-  sort "/tmp/alttp - VT_no-glitches-27_normal_open_none_${seed}.txt" \
+  php artisan -v alttp:randomize --skip-md5 /tmp/rom /tmp --no-rom --spoiler --seed=${seed} --mode=open \
+    | fgrep " := " \
     | egrep -v "(Waterfall|Pyramid)Bottle" \
+    | egrep -v "= Defeat|PyramidFairy(Bow|Sword)" \
     > /tmp/php
 
-  app/bazel-bin/makeseed "${seed}" | \
-    sort | \
-    egrep -v "= Defeat|PyramidFairy(Bow|Sword)" \
+  app/bazel-bin/makeseed "${seed}" \
+    | fgrep " := " \
+    | fgrep -v "Prize :=" \
+    | egrep -v "= Defeat|PyramidFairy(Bow|Sword)" \
     > /tmp/c
   diff -u /tmp/php /tmp/c
 }
