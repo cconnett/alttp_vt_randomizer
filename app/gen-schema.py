3a852a4af119c9cda0d3de66705328b58d023e55
Chris Connett <cjc@google.com>
2018-03-05 19:02:30 -0500

    Use integer primary key.

--- app/gen-schema.py
+++ app/gen-schema.py
@@ -3,7 +3,8 @@
 from __future__ import print_function
 
 tmpl = ('CREATE TABLE IF NOT EXISTS '
-        'seeds_with_item{item:03d}_at_location{location:03d} (seed int);')
+        'seeds_with_item{item:03d}_at_location{location:03d} '
+        '(seed integer primary key);')
 f = open('seeds.schema', 'w')
 for item in range(1, 157):
   for location in range(1, 235):
