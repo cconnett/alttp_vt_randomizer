5b84c1c902ebf4080ce3b887369e7d02ebc44bee
Chris Connett <cjc@google.com>
2018-03-07 16:41:01 -0500

    Use a work queue instead of fixed work shares.

--- app/makeseed.cc
+++ app/makeseed.cc
@@ -1,7 +1,9 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <cstring>
+#include <deque>
 #include <iostream>
+#include <mutex>
 #include <vector>
 
 #include "arraylength.h"
@@ -18,14 +20,12 @@ using namespace std;
 #define TRANSACTION_SIZE (1000)
 #define DB_FILENAME ("seeds.db")
 
-struct range {
-  int begin;
-  int end;
-};
+deque<int> work;
+mutex m_work;
+int stop;
 
-void *populate_seed_range(void *range_struct) {
-  struct range myrange = *(struct range *)range_struct;
-  cout << "Thread with range " << myrange.begin << "-" << myrange.end << endl;
+void *populate_seed_range(void *unused) {
+  cout << "Warming up" << endl;
 
   sqlite3 *conn;
   int status = sqlite3_open(DB_FILENAME, &conn);
@@ -33,10 +33,15 @@ void *populate_seed_range(void *range_struct) {
     cerr << "Cannot open DB." << endl;
     exit(status);
   }
+  sqlite3_busy_timeout(conn, 180000);
+
+  sqlite3_stmt *begin, *commit;
+  sqlite3_prepare_v2(conn, "BEGIN;", 256, &begin, nullptr);
+  sqlite3_prepare_v2(conn, "COMMIT;", 256, &commit, nullptr);
 
   sqlite3_stmt *stmt[(int)Item::NUM_ITEMS][(int)Location::NUM_LOCATIONS];
+
   char buffer[256];
-  cout << "lo" << endl;
   for (int item = 1; item < (int)Item::NUM_ITEMS; item++) {
     for (int location = 1; location < (int)Location::NUM_LOCATIONS;
          location++) {
@@ -47,28 +52,44 @@ void *populate_seed_range(void *range_struct) {
       sqlite3_prepare_v2(conn, buffer, 256, &stmt[item][location], nullptr);
     }
   }
-  cout << "hi" << endl;
 
-  for (int base = myrange.begin; base < myrange.end; base += TRANSACTION_SIZE) {
+  int base;
+  while (true) {
+    {
+      lock_guard<mutex> lock(m_work);
+      if (work.empty()) {
+        break;
+      }
+      base = work.front();
+      work.pop_front();
+    }
+    cout << "Working on " << base << "-" << min(base + TRANSACTION_SIZE, stop)
+         << endl;
+
     World *result[TRANSACTION_SIZE];
-    for (int offset = 0;
-         offset < TRANSACTION_SIZE && base + offset < myrange.end; offset++) {
+    for (int offset = 0; offset < TRANSACTION_SIZE && base + offset < stop;
+         offset++) {
       try {
         result[offset] = new World(base + offset);
       } catch (CannotPlaceItem &e) {
         exit(1);
       }
     }
-    sqlite3_exec(conn, "BEGIN TRANSACTION;", nullptr, nullptr, nullptr);
-    for (int offset = 0;
-         offset < TRANSACTION_SIZE && base + offset < myrange.end; offset++) {
+    sqlite3_step(begin);
+    for (int offset = 0; offset < TRANSACTION_SIZE && base + offset < stop;
+         offset++) {
       result[offset]->sqlite3_write(stmt, base + offset);
       delete result[offset];
     }
-    sqlite3_exec(conn, "COMMIT TRANSACTION;", nullptr, nullptr, nullptr);
-    cout << "Completed " << base << "-"
-         << min(base + TRANSACTION_SIZE, myrange.end) << endl;
+    sqlite3_step(commit);
+    sqlite3_reset(begin);
+    sqlite3_reset(commit);
+
+    cout << "Completed " << base << "-" << min(base + TRANSACTION_SIZE, stop)
+         << endl;
   }
+
+  cout << "No more work. Shutting down." << endl;
   sqlite3_close(conn);
   return nullptr;
 }
@@ -90,17 +111,18 @@ int main(int argc, char **argv) {
     return 2;
   }
 
-  int begin = 1, end = 50000;
-  begin = atoi(argv[1]);
-  end = atoi(argv[2]);
+  int start = 1;
+  stop = 50000;
+  start = atoi(argv[1]);
+  stop = atoi(argv[2]);
 
-  struct range ranges[THREADS];
   pthread_t threads[THREADS];
 
+  for (int block = start; block < stop; block += TRANSACTION_SIZE) {
+    work.push_back(block);
+  }
   for (int t = 0; t < THREADS; t++) {
-    ranges[t].begin = begin + (end - begin) * t / THREADS;
-    ranges[t].end = begin + (end - begin) * (t + 1) / THREADS;
-    pthread_create(&threads[t], nullptr, populate_seed_range, &ranges[t]);
+    pthread_create(&threads[t], nullptr, populate_seed_range, nullptr);
   }
   for (int t = 0; t < THREADS; t++) {
     pthread_join(threads[t], nullptr);
