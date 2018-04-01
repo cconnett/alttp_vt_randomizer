62bc4af68fcb8d106f71372396e182dc55e531f0
Chris Connett <chris@connett.net>
2018-04-01 02:39:59 -0400

    Bring the new generators back in with a compile-time option to select compatibility.
    This reverts commit 560278dc39fc9a6ee1f5a7b7f7f45490e0e0966a.

--- app/mt_rand.cc
+++ app/mt_rand.cc
@@ -2,30 +2,58 @@
 #include <iostream>
 
 #include "mt_rand.h"
-#include "prngs/PHP_mt19937.h"
+#include "prngs/Mt19937.h"
 
-uint scale(uint n, uint min, uint max) {
-  return min + (uint)((max - min + 1.0) * (((double)n) / 0x80000000u));
-}
+// PHP 7.0 has the bad algorithm and bad scaling.
+// PHP 7.1 has the good algorithm and bad scaling.
+// PHP 7.2 has the good algorithm and good scaling.
+// #define BADSCALING
+// #define BADGENERATOR
 
-unsigned int mt_rand::rand(uint min, uint max) {
-  uint raw = ((PHP_mt19937 *)generator)->random();
-  uint ret = scale(raw, min, max);
-#ifdef RAND_SYNC
-  std::cout << "mt_rand(" << min << ", " << max << ") = " << ret << std::endl;
+#ifdef BADGENERATOR
+typedef PHP_mt19937 Generator;
+#else
+typedef Mt19937 Generator;
 #endif
-  assert(min <= ret);
-  assert(ret <= max);
+
+#ifdef BADSCALING
+unsigned int mt_rand::rand(uint min, uint max) {
+  uint raw = ((Generator *)generator)->random();
+  uint ret = min + (uint)((max - min + 1.0) * (((double)raw) / 0x100000000ul));
+#ifdef RAND_SYNC
+  std::cout << "mt_rand(" << min << ", " << max + min << ") = " << ret
+            << std::endl;
+#endif
   return ret;
 }
+#else
+unsigned int mt_rand::rand(uint min, uint max) {
+  max -= min;
+  max++;
+
+  uint result = ((Generator *)generator)->random();
+  if ((max & (max - 1)) == 0) {
+    return result & (max - 1);
+  }
+  uint limit = 0xffffffff - (0xffffffff % max) - 1;
+  while (result > limit) {
+    result = ((Generator *)generator)->random();
+  }
+#ifdef RAND_SYNC
+  std::cout << "mt_rand(" << min << ", " << max + min
+            << ") = " << result % max + min << std::endl;
+#endif
+  return result % max + min;
+}
+#endif
 
 mt_rand::mt_rand(int seed) {
-  generator = new PHP_mt19937;
-  ((PHP_mt19937 *)generator)->seed(seed);
+  generator = new Mt19937;
+  ((Mt19937 *)generator)->seed(seed);
 }
 
 mt_rand::~mt_rand() {
   if (generator) {
-    delete (PHP_mt19937 *)generator;
+    delete (Mt19937 *)generator;
   }
 }
