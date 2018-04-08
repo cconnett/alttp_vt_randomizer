797f1ed9f5cd43428c67113ac1b1f7933345d615
Chris Connett <chris@connett.net>
2018-04-08 13:43:22 -0400

    Improvements to user_results script.

--- app/user_results.py
+++ app/user_results.py
@@ -1,41 +1,41 @@
 import array
 import ctypes
 import os
+import subprocess
 
 import mappings
 
-item_location_pairs = [
-  ('Bombos', 'LinksUncle')
+location_item_pairs = [
   ]
 
 candidates = set()
-for (item, location) in item_location_pairs:
-  print(item, location)
+for (location, item) in location_item_pairs:
+  print(location, item)
   if isinstance(item, str):
     item = mappings.items[item]
   if isinstance(location, str):
     location = mappings.locations[location]
-  print(item, location)
-  path = 'bigresults/item{:03d}-location{:03d}'.format(item, location)
+  print(f'{location} has {item}')
   matches = set()
-  for filename in os.listdir(path):
-    #buf = ctypes.create_string_buffer(open(os.path.join(path, filename), 'rb').read())
-    a = array.array('I')
-    try:
-      a.fromfile(open(os.path.join(path, filename), 'rb'), 8 * 1024 * 1024)
-    except EOFError:
-      pass
-    print(len(a))
-    matches |= set(a)
-  print(len(matches))
+  for batch in range(1, 7):
+    path = f'bigresults/batch{batch}/item{item:03d}-location{location:03d}'
+    for filename in os.listdir(path):
+      a = array.array('I')
+      try:
+        a.fromfile(open(os.path.join(path, filename), 'rb'), 8 * 1024 * 1024)
+      except EOFError:
+        pass
+      matches |= set(a)
   if not candidates:
     candidates = matches
   else:
     candidates &= matches
   if len(candidates) == 1:
-    print('Match found: {}'.format(list(candidates)[0]))
+    winner = list(candidates)[0]
+    subprocess.call('./bazel-bin/makeseed {winner}'.format(winner=winner), shell=True)
+    print('Match found: {winner}'.format(winner=winner))
     break
-else:
-  print('{} seeds remaining.'.format(len(candidates)))
-  if len(candidates) <= 10:
-    print(candidates)
+  else:
+    print('{} seeds remaining.'.format(len(candidates)))
+    if len(candidates) <= 10:
+      print(candidates)
